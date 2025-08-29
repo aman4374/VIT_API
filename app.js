315a0f0d27dae9fdd67776d44f0243a557069017
Aman Trivedi <amantrivedi4374@gmail.com>
2025-08-29 11:58:26 +0530

    update app.js

--- app.js
+++ app.js
@@ -13,8 +13,15 @@ app.use(cors());
 app.use(morgan('combined'));
 app.use(express.json({ limit: '10mb' }));
 
+// Root route
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: "API is running. Use POST /bfhl to test."
+  });
+});
+
 // Routes
-app.use('/', bfhlRouter);
+app.use('/bfhl', bfhlRouter);
 
 // Global error handler
 app.use((err, req, res, next) => {
@@ -24,15 +31,8 @@ app.use((err, req, res, next) => {
     error: 'Internal server error'
   });
 });
-// Root route
-app.get('/', (req, res) => {
-  res.status(200).json({
-    message: "API is running. Use POST /bfhl to test."
-  });
-});
 
-
-// 404 handler
+// 404 handler (keep last)
 app.use('*', (req, res) => {
   res.status(404).json({
     is_success: false,
